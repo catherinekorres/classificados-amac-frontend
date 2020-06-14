c68baef1a22bfa1258bf9ed44f2f76061e965be8
Catherine Korres <cathy.korres@gmail.com>
2020-06-14 18:08:03 -0300

    Criando página Login

---
+++ src/pages/Login/index.js
@@ -0,0 +1,151 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from 'react';
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+
+// core components
+import CustomNavbar from '../../components/argon/CustomNavbar.js';
+import Footer from '../../components/argon/Footer.js';
+
+class Login extends React.Component {
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+
+  render() {
+    return (
+      <>
+        <CustomNavbar />
+        <main ref="main">
+          <section
+            className="section section-shaped section-lg"
+            style={{ minHeight: '75vh' }}
+          >
+            <div className="shape shape-style-1 bg-gradient-default">
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+            <Container className="pt-lg-7">
+              <Row className="justify-content-center">
+                <Col lg="5">
+                  <Card className="bg-secondary shadow border-0">
+                    <CardBody className="px-lg-5 pb-lg-5">
+                      <div className="text-center text-muted mb-5">
+                        <h4 className="font-weight-bold">Faça seu login</h4>
+                      </div>
+                      <Form role="form">
+                        <FormGroup className="mb-3">
+                          <InputGroup className="input-group-alternative">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-email-83" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input placeholder="Email" type="email" />
+                          </InputGroup>
+                        </FormGroup>
+                        <FormGroup>
+                          <InputGroup className="input-group-alternative">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-lock-circle-open" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                              placeholder="Senha"
+                              type="password"
+                              autoComplete="off"
+                            />
+                          </InputGroup>
+                        </FormGroup>
+                        {/* <div className="custom-control custom-control-alternative custom-checkbox">
+                          <input
+                            className="custom-control-input"
+                            id=" customCheckLogin"
+                            type="checkbox"
+                          />
+                          <label
+                            className="custom-control-label"
+                            htmlFor=" customCheckLogin"
+                          >
+                            <span>Remember me</span>
+                          </label>
+                        </div> */}
+                        <div className="text-center">
+                          <Button
+                            className="my-4 btn-block"
+                            color="primary"
+                            type="button"
+                          >
+                            Entrar
+                          </Button>
+                        </div>
+                      </Form>
+                    </CardBody>
+                  </Card>
+                  <Row className="mt-3">
+                    <Col xs="6">
+                      <a
+                        className="text-light"
+                        href="#"
+                        onClick={e => e.preventDefault()}
+                      >
+                        <small>Esqueceu sua senha?</small>
+                      </a>
+                    </Col>
+                    <Col className="text-right" xs="6">
+                      <a className="text-light" href="/cadastro">
+                        <small>Criar nova conta</small>
+                      </a>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </main>
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default Login;
