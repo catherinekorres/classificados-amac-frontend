4ae23f939e0541d3e34f344e7e448ab043bbd81e
Catherine Korres <cathy.korres@gmail.com>
2020-06-14 17:01:32 -0300

    Organizando Main em componentes separados

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,59 +1,38 @@
 import React, { Component } from 'react';
 
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import Hero from '../../components/argon/Hero';
+import CustomNavbar from '../../components/argon/CustomNavbar';
+import Footer from '../../components/argon/Footer';
 
-import api from '../../services/api';
-
-const formatter = new Intl.NumberFormat('pt-BR', {
-  style: 'currency',
-  currency: 'BRL',
-});
+import Introduction from './Sections/Introduction';
+import JoinTheCause from './Sections/JoinTheCause';
+import RecentProducts from './Sections/RecentProducts';
+import RecentServices from './Sections/RecentServices';
 
 export default class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { products: [] };
-  }
-
   async componentDidMount() {
-    try {
-      const response = await api.get(`/products`);
-
-      console.log(response);
-
-      response.data.forEach(product => {
-        const { products } = this.state;
-        this.setState({
-          products: [...products, product],
-        });
-      });
-    } catch (error) {
-      alert(error);
-    }
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
   }
 
   render() {
-    const { products } = this.state;
-
     return (
-      <Container>
-        <Row>
-          {products.map((product, i) => (
-            <Col md={4} key={i}>
-              <Card className="mt-5">
-                <Card.Body>
-                  <Card.Title>{product.name}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    {formatter.format(product.price)}
-                  </Card.Subtitle>
-                  <Card.Text>{product.description}</Card.Text>
-                  <Card.Link href="#">Ver mais ></Card.Link>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      <>
+        <CustomNavbar />
+        <main ref="main">
+          <Hero />
+
+          <Introduction />
+
+          <JoinTheCause />
+
+          <RecentProducts />
+
+          <RecentServices />
+        </main>
+        <Footer />
+      </>
     );
   }
 }
