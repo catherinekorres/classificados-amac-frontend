25b8324b323eca6e1144fa5d95d11832c8771f81
Catherine Korres <cathy.korres@gmail.com>
2020-06-14 16:58:53 -0300

    Adicionando estilos ao App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 import Routes from './routes';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/vendor/nucleo/css/nucleo.css';
+import './assets/vendor/font-awesome/css/font-awesome.min.css';
+import './assets/scss/argon-design-system-react.scss';
 
 function App() {
   return (
