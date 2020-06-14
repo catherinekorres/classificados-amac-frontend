3b1f833b77ccafe89162e6ab6acdf4ea31cfa71d
Catherine Korres <cathy.korres@gmail.com>
2020-06-14 18:08:17 -0300

    Criando página Produtos

---
+++ src/pages/Produtos/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import Hero from '../../components/argon/Hero';
+import CustomNavbar from '../../components/argon/CustomNavbar';
+import Footer from '../../components/argon/Footer';
+
+export default class Produtos extends Component {
+  async componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+
+  render() {
+    return (
+      <>
+        <CustomNavbar />
+        <main ref="main">
+          <Hero />
+          Produtos
+        </main>
+        <Footer />
+      </>
+    );
+  }
+}
