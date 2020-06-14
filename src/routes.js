14f566561aa0594648894f3b01f9bb3e6365dc4f
Catherine Korres <cathy.korres@gmail.com>
2020-06-14 18:01:52 -0300

    Adicionando rotas mapeadas

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
+import Produtos from './pages/Produtos';
+import Servicos from './pages/Servicos';
+import Contato from './pages/Contato';
+import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main} />
+        <Route path="/produtos" component={Produtos} />
+        <Route path="/servicos" component={Servicos} />
+        <Route path="/contato" component={Contato} />
+        <Route path="/login" component={Login} />
+        <Route path="/cadastro" component={Cadastro} />
       </Switch>
     </BrowserRouter>
   );
