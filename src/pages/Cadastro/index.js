7eb178a56bed8d4970767756fc5c26bd95c4a6c1
Catherine Korres <cathy.korres@gmail.com>
2020-06-14 18:07:57 -0300

    Criando página Cadastro

---
+++ src/pages/Cadastro/index.js
@@ -0,0 +1,132 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from 'react';
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+
+// core components
+import CustomNavbar from '../../components/argon/CustomNavbar.js';
+import Footer from '../../components/argon/Footer.js';
+
+class Cadastro extends React.Component {
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+
+  render() {
+    return (
+      <>
+        <CustomNavbar />
+        <main ref="main">
+          <section
+            className="section section-shaped section-lg"
+            style={{ minHeight: '75vh' }}
+          >
+            <div className="shape shape-style-1 bg-gradient-default">
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+            <Container className="pt-lg-7">
+              <Row className="justify-content-center">
+                <Col lg="5">
+                  <Card className="bg-secondary shadow border-0">
+                    <CardBody className="px-lg-5 pb-lg-5">
+                      <div className="text-center text-muted mb-5">
+                        <h4 className="font-weight-bold">Cadastre-se</h4>
+                      </div>
+                      <Form role="form">
+                        <FormGroup>
+                          <InputGroup className="input-group-alternative mb-3">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-hat-3" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input placeholder="Nome" type="text" />
+                          </InputGroup>
+                        </FormGroup>
+                        <FormGroup>
+                          <InputGroup className="input-group-alternative mb-3">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-email-83" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input placeholder="Email" type="email" />
+                          </InputGroup>
+                        </FormGroup>
+                        <FormGroup>
+                          <InputGroup className="input-group-alternative">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-lock-circle-open" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input
+                              placeholder="Senha"
+                              type="password"
+                              autoComplete="off"
+                            />
+                          </InputGroup>
+                        </FormGroup>
+                        <div className="text-center">
+                          <Button
+                            className="mt-4 btn-block"
+                            color="primary"
+                            type="button"
+                          >
+                            Criar conta
+                          </Button>
+                        </div>
+                      </Form>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </main>
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default Cadastro;
